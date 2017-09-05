204a0215f680cffbf9153db3efae0327b3ca2409
taojun.li <taojun.li@rwth-aachen.de>
2017-09-06 00:47:38 +0200

    add lock, ui, controller

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import os
 from Tkinter import *
 import tkFileDialog
+import threading
 
 DEFAULT_DIR = 'D:/TestTools/Logs/'
 
@@ -15,18 +16,56 @@ TASK = 'Task'
 COMPLETED = 'Completed'
 ERROR = 'ERROR'
 
+class UI(Frame):
 
-class LogCal(Frame):
     def __init__(self, master=None):
 
         Frame.__init__(self, master)
+        self.root = master
+
         self.grid()
         self.master.title("Log Analyzer")
         self.master.rowconfigure(0, weight=1)
         self.master.columnconfigure(0, weight=1)
+        self._createWidgets()
 
-        
-        self.root = master
+    def _createWidgets(self):
+
+        self.FROM = Label(self, text='From: ', bg="black", fg="white")
+        self.FROM.grid(row = 0, column = 1, rowspan = 3, columnspan = 2, sticky = W+E+N+S)
+        self.FROM.pack(fill=BOTH)
+
+        self.To = Label(self, text='To: ', bg="black", fg="white")
+        self.To.pack(fill=BOTH)
+
+        self.Error = Label(self, text='Errors number: ', bg="red", fg="white")
+        self.Error.pack(fill=BOTH)
+
+        self.Count = Label(self, text='Completed requests number: ', bg="red", fg="white")
+        self.Count.pack(fill=BOTH)
+
+        self.Seconds = Label(self, text='Total seconds: ', bg="red", fg="white")
+        self.Seconds.pack(fill=BOTH)
+
+        self.Result = Label(self, text='Request per second: '+ ' r/s', bg="red", fg="white")
+        self.Result.pack(fill=BOTH)
+
+        self.Open = Button(self, text='Open')
+        self.Open.pack(side=RIGHT, fill=X)
+
+        self.Run = Button(self, text='Run')
+        self.Run.pack(side=RIGHT , fill=X)
+
+        self.listb  = Listbox(self)
+        self.listb.pack(side = RIGHT, fill=BOTH, expand=5)
+
+class Controller(threading.Thread):
+
+    def __init__(self, lock, view = UI):
+
+        self.lock = lock
+        self.view = view
+
         self.files = []
         self.date = ''
         self.count = 0
@@ -37,43 +76,26 @@ class LogCal(Frame):
         self.dt_delta = 0
         self.rps = 0
 
-        self.createWidgets()
-
-    def createWidgets(self):
-
-
-        self.FROM = Label(self, text='From: ' + str(self.date_from), bg="black", fg="white")
-        self.FROM.grid(row = 0, column = 1, rowspan = 3, columnspan = 2, sticky = W+E+N+S)
-        self.FROM.pack(fill=BOTH)
-
-        self.To = Label(self, text='To: ' + str(self.date_to), bg="black", fg="white")
-        self.To.pack(fill=BOTH)
-
-        self.Error = Label(self, text='Errors number: ' + str(self.error), bg="white", fg="red")
-        self.Error.pack(fill=BOTH)
-
-        self.Count = Label(self, text='Completed requests number: ' + str(self.count), bg="red", fg="white")
-        self.Count.pack(fill=BOTH)
-
-        self.Seconds = Label(self, text='Total seconds: ' + str(self.dt_delta), bg="red", fg="white")
-        self.Seconds.pack(fill=BOTH)
-
-        self.Result = Label(self, text='Request per second: ' + '%.2f' % self.rps + ' r/s', bg="red", fg="white")
-        self.Result.pack(fill=BOTH)
-
-
-        self.Open = Button(self, text='Open', command=self.callback)
-        self.Open.pack(side=RIGHT, fill=X)
-
-        self.Run = Button(self, text='Run', command=self.run)
-        self.Run.pack(side=RIGHT , fill=X)
-
-        self.listb  = Listbox(self) 
-        self.listb.pack(side = RIGHT, fill=BOTH, expand=5)
-
+        self._updateWidgets()
         self._initListbox()
 
-    def _refreshList(self):
+    def _updateWidgets(self):
+
+        self.view.FROM.configure(text='From: ' + str(self.date_from))
+        self.view.To.configure(text='To: ' + str(self.date_to))
+        self.view.Error.configure(text='Errors number: ' + str(self.error))
+        self.view.Count.configure(text='Completed requests number: ' + str(self.count))
+        self.view.Seconds.configure(text='Total seconds: ' + str(self.dt_delta))
+        self.view.Result.configure(text='Request per second: ' + '%.2f' % self.rps + ' r/s')
+        self.view.Open.configure(command=self.callback)
+        self.view.Run.configure(command=self.run)
+
+    def _reset(self):
+
+        self._resetList()
+        self._updateFiles()
+
+    def _resetList(self):
 
         self.date = ''
         self.count = 0
@@ -83,40 +105,41 @@ class LogCal(Frame):
         self.date_to = 0
         self.dt_delta = 0
         self.rps = 0
-        self._updateFiles()
 
     def _initListbox(self):
         list_dirs = os.walk(DEFAULT_DIR)
         for root, dirs, files in list_dirs:
             for f in files:
-                log = os.path.join(root, f)  
-                self.listb.insert(0,log)
+                log = os.path.join(root, f)
+                self.view.listb.insert(0,log)
                 self.files.append(log)
 
-    def refresh(self):
+    def _refresh(self):
 
-        self.FROM.configure(text='From: ' + str(self.date_from))
-        self.To.configure(text='To: ' + str(self.date_to))
-        self.Error.configure(text='Errors number: ' + str(self.error))
-        self.Count.configure(text='Completed requests number: ' + str(self.count))
-        self.Seconds.configure(text='Total seconds: ' + str(self.dt_delta))
-        self.Result.configure(text='Request per second: ' + '%.2f' % self.rps + ' r/s')
-        self.root.after(1500, self.refresh)
+        self._updateWidgets()
+        self.view.after(1500, self._refresh)
 
     def _updateFiles(self):
-        self.listb.delete(0, END)
-        for f in self.files:
-            self.listb.insert(0,f)
 
-    def _logCheck(self, rootDir):
-        if self.files:
-            for f in self.files:
-                print f
-                self._processLog(f)
-        else:
-            return False
+        self.view.listb.delete(0, END)
+        for f in self.files:
+            self.view.listb.insert(0,f)
+
+    def _onButton(self):
+
+        self.view.Run.configure(stat='active')
+        self.view.Open.configure(stat='active')
+
+    def _offButton(self):
+
+        self.view.Run.configure(stat='disable')
+        self.view.Open.configure(stat='disable')
 
     def _processLog(self, file):
+
+        self.lock.acquire()
+        self._offButton()
+
         try:
             log = open(file, 'r')
             lines = log.readlines()
@@ -134,12 +157,16 @@ class LogCal(Frame):
         except IOError:
             print('Log File Error!')
             return False
+
         finally:
             log.close()
+            self.lock.release()
+            self._onButton()
 
     def _rpsCal(self):
 
         if self.record:
+
             record = sorted(self.record, key=lambda x: x["datetime"])
             self.date_from = record[0]['datetime']
             self.date_to = record[-1]['datetime']
@@ -155,30 +182,43 @@ class LogCal(Frame):
 
     def _selectFiles(self):
 
-        filez = tkFileDialog.askopenfilenames(parent=self.root, title='Choose a file',initialdir = DEFAULT_DIR)
-        self.files = self.root.tk.splitlist(filez)
+        filez = tkFileDialog.askopenfilenames(parent=self.view.root, title='Choose a file',initialdir = DEFAULT_DIR)
+        self.files = self.view.tk.splitlist(filez)
+
+    def _logCheck(self, rootDir=None):
+
+        if self.files:
+            for f in self.files:
+                print f
+                self._processLog(f)
+        else:
+            return False
 
     def logCheck(self):
+
         if len(sys.argv) < 1:
             self._logCheck(sys.argv[1])
         else:
             self._logCheck(DEFAULT_DIR)
+
         self._rpsCal()
 
     def callback(self):
-        self._refreshList()
+
+        self._reset()
         self._selectFiles()
         self._updateFiles()
 
     def run(self):
 
-        self._refreshList()
+        self._reset()
         self.thread = Thread(target=self.logCheck)
         self.thread.start()
-        self.refresh()
+        self._refresh()
 
-    
+
 root = Tk()
-app = LogCal(master=root)
-app.mainloop()
-root.destroy()
+ui = UI(root)
+lock = threading.Lock()
+app = Controller(lock, view = ui)
+app.view.mainloop()
